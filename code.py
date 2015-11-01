ab8cf94bf14e6224d523934b50f81a56cff1fd94
JuPeg <juliana_pegoraro@hotmail.com>
2015-11-01 15:45:58 +0100

    code returns results file

--- code.py
+++ code.py
@@ -1,8 +1,12 @@
 from Bio import SeqIO
 count = 0
+
+f = open("results.txt","w") # open "results" file
+
 # iterating through all sequences in fasta file
 for seq in SeqIO.parse("sequences.fasta","fasta"):
 	count = count + 1 # number of sequences
-	print(seq.id +" \t  "+ str(len(seq))) # sequences names and lengths
-print("\nThere are "+str(count)+" sequences")
+	f.write(seq.id +" \t  "+ str(len(seq)) + "\n") # sequences names and lengths
+f.write("\nThere are "+str(count)+" sequences")
 
+f.close() # close results file
